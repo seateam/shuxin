531bf86b03f2801a57c908b50e429781fe70e2fe
大海 <c@bigc.cc>
2019-05-10 02:07:16 +0800

    你难过的太假装

--- app/pages/card/card.js
+++ app/pages/card/card.js
@@ -4,8 +4,37 @@ Page({
 	data: {
 		mark: app.data.mark,
 	},
-	onLoad() {},
-	onShow() {},
+	onLoad() {
+		// 获取周边位置
+		wx.getLocation({
+			type: 'gcj02',
+			success: res => {
+				const lat = res.latitude
+				const lng = res.longitude
+				const meter = 500
+				Sea.Ajax({
+					url: 'https://apis.map.qq.com/ws/place/v1/search',
+					data: {
+						boundary: `nearby(${lat},${lng},${meter})`,
+						key: 'IHRBZ-RVWWQ-FHC5D-GGHIX-WPIGS-MABL6',
+					},
+				}).then(res => {
+					Sea.cardLocation = res.data
+					const location = res.data[0]
+					this.setData({
+						location: location,
+					})
+				})
+			},
+		})
+	},
+	onShow() {
+		if (Sea.cardLocationNow !== undefined) {
+			this.setData({
+				location: Sea.cardLocation[Sea.cardLocationNow],
+			})
+		}
+	},
 	bindColor(event) {
 		const i = event.target.dataset.i
 		const mark = app.data.mark
@@ -14,7 +43,7 @@ Page({
 			mark: mark,
 		})
 	},
-	bindLocation(){
+	bindLocation() {
 		Sea.path('/pages/cardLocation/cardLocation')
-	}
+	},
 })
--- app/pages/cardLocation/cardLocation.js
+++ app/pages/cardLocation/cardLocation.js
@@ -3,28 +3,20 @@ const Sea = require('../../ku/bigsea.js')
 Page({
 	data: {
 		list: [],
+		now: 0,
 	},
 	onLoad() {
-		// 获取周边位置
-		wx.getLocation({
-			type: 'gcj02',
-			success: res => {
-				const lat = res.latitude
-				const lng = res.longitude
-				const meter = 500
-				Sea.Ajax({
-					url: 'https://apis.map.qq.com/ws/place/v1/search',
-					data: {
-						boundary: `nearby(${lat},${lng},${meter})`,
-						key: 'IHRBZ-RVWWQ-FHC5D-GGHIX-WPIGS-MABL6',
-					},
-				}).then(res => {
-					this.setData({
-						list: res.data,
-					})
-				})
-			},
+		this.setData({
+			list: Sea.cardLocation,
+			now: Sea.cardLocationNow || 0,
 		})
 	},
 	onShow() {},
+	bindOne(event) {
+		const i = event.currentTarget.dataset.i
+		this.setData({
+			now: i,
+		})
+		Sea.cardLocationNow = i
+	},
 })
