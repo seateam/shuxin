b062de707dbb76f8dff9888ca6c338acd28f4b8f
大海 <c@bigc.cc>
2019-05-09 23:55:32 +0800

    card

--- app/app.js
+++ app/app.js
@@ -1,9 +1,20 @@
 const Sea = require('./ku/bigsea.js')
 App({
-  data: {
-    location: null,
-  },
-  onLaunch() {},
-  onShow() {},
-  onHide() {},
+	data: {
+		location: null,
+		mark: {
+			arr: [
+				'#F82727',
+				'#E88536',
+				'#F9EE33',
+				'#32B16C',
+				'#448ACA',
+				'#8957A1',
+			],
+			now: 0,
+		},
+	},
+	onLaunch() {},
+	onShow() {},
+	onHide() {},
 })
--- app/pages/card/card.js
+++ app/pages/card/card.js
@@ -1,6 +1,17 @@
 const app = getApp()
 const Sea = require('../../ku/bigsea.js')
 Page({
-  onLoad() {},
-  onShow() {}
+	data: {
+		mark: app.data.mark,
+	},
+	onLoad() {},
+	onShow() {},
+	bindColor(event) {
+		const i = event.target.dataset.i
+		const mark = app.data.mark
+		mark.now = i
+		this.setData({
+			mark: mark,
+		})
+	},
 })
