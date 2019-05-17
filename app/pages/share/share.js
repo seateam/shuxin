73a149dbe2dde68a4aa5ca4ea217ef0bf92f9509
大海 <c@bigc.cc>
2019-05-17 16:49:36 +0800

    share

--- app/pages/share/share.js
+++ app/pages/share/share.js
@@ -1,6 +1,18 @@
 const app = getApp()
 const Sea = require('../../ku/bigsea.js')
 Page({
-  onLoad() {},
-  onShow() {},
+	data: {
+		shareIndex: 1,
+	},
+	onLoad() {},
+	onShow() {},
+	bindNext() {
+    let i = this.data.shareIndex + 1
+    if (i === 5) {
+      i = 1
+    }
+    this.setData({
+			shareIndex: i,
+		})
+  },
 })
