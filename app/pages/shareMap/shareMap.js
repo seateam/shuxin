67e0169f65cddf8bcc2a86d82c46cb7ee0fe150f
大海 <c@bigc.cc>
2019-05-20 22:00:30 +0800

    520_2

--- app/pages/shareMap/shareMap.js
+++ app/pages/shareMap/shareMap.js
@@ -1,38 +1,111 @@
+import * as echarts from './ec-canvas/echarts'
+import geoJson from './mapData.js'
+
 const app = getApp()
-const Sea = require('../../ku/bigsea.js')
+
+function initChart(canvas, width, height) {
+	const chart = echarts.init(canvas, null, {
+		width: width,
+		height: height,
+	})
+	canvas.setChart(chart)
+
+	echarts.registerMap('henan', geoJson)
+
+	const option = {
+		tooltip: {
+			trigger: 'item',
+		},
+
+		visualMap: {
+			min: 0,
+			max: 100,
+			left: 'left',
+			top: 'bottom',
+			text: ['高', '低'], // 文本，默认为数值文本
+			calculable: true,
+		},
+		toolbox: {
+			show: true,
+			orient: 'vertical',
+			left: 'right',
+			top: 'center',
+			feature: {
+				dataView: { readOnly: false },
+				restore: {},
+				saveAsImage: {},
+			},
+		},
+		series: [
+			{
+				type: 'map',
+				mapType: 'henan',
+				label: {
+					normal: {
+						show: true,
+					},
+					emphasis: {
+						textStyle: {
+							color: '#fff',
+						},
+					},
+				},
+				itemStyle: {
+					normal: {
+						borderColor: '#389BB7',
+						areaColor: '#fff',
+					},
+					emphasis: {
+						areaColor: '#389BB7',
+						borderWidth: 0,
+					},
+				},
+				animation: false,
+
+				data: [
+					{ name: '郑州市', value: 100 },
+					{ name: '洛阳市', value: 10 },
+					{ name: '开封市', value: 20 },
+					{ name: '信阳市', value: 30 },
+					{ name: '驻马店市', value: 40 },
+					{ name: '南阳市', value: 41 },
+					{ name: '周口市', value: 15 },
+					{ name: '许昌市', value: 25 },
+					{ name: '平顶山市', value: 35 },
+					{ name: '新乡市', value: 35 },
+					{ name: '漯河市', value: 35 },
+					{ name: '商丘市', value: 35 },
+					{ name: '三门峡市', value: 35 },
+					{ name: '济源市', value: 35 },
+					{ name: '焦作市', value: 35 },
+					{ name: '安阳市', value: 35 },
+					{ name: '鹤壁市', value: 35 },
+					{ name: '濮阳市', value: 35 },
+					{ name: '开封市', value: 45 },
+				],
+			},
+		],
+	}
+
+	chart.setOption(option)
+
+	return chart
+}
+
 Page({
-	data: {
-		shareIndex: 1,
-		data: {},
-	},
-	onLoad() {
-		Sea.Ajax({
-			url: '/v1/card.share',
-		}).then(res => {
-			if (res.ok) {
-				const data = Object.assign(res.data[0], res.data[1])
-				for (const key in data) {
-					if (typeof data[key] === 'string') {
-						data[key] = Sea.formatCity(data[key])
-					}
-				}
-				this.setData({
-					data: data,
-				})
-			}
-		})
-	},
-	onShow() {},
-	bindNext() {
-		let i = this.data.shareIndex + 1
-		if (i === 5) {
-			i = 1
+	onShareAppMessage: function(res) {
+		return {
+			title: 'ECharts 可以在微信小程序中使用啦！',
+			path: '/pages/index/index',
+			success: function() {},
+			fail: function() {},
 		}
-		this.setData({
-			shareIndex: i,
-		})
 	},
-	bindHoliday(){
-		Sea.path('/pages/holiday/holiday')
+	data: {
+		ec: {
+			onInit: initChart,
+		},
 	},
+
+	onReady() {},
 })
