f48aba6f4a9a63d09e527cf478992696e6b8f1e9
大海 <c@bigc.cc>
2019-05-20 21:45:55 +0800

    520_1

---
+++ app/pages/shareMap/shareMap.js
@@ -0,0 +1,38 @@
+const app = getApp()
+const Sea = require('../../ku/bigsea.js')
+Page({
+	data: {
+		shareIndex: 1,
+		data: {},
+	},
+	onLoad() {
+		Sea.Ajax({
+			url: '/v1/card.share',
+		}).then(res => {
+			if (res.ok) {
+				const data = Object.assign(res.data[0], res.data[1])
+				for (const key in data) {
+					if (typeof data[key] === 'string') {
+						data[key] = Sea.formatCity(data[key])
+					}
+				}
+				this.setData({
+					data: data,
+				})
+			}
+		})
+	},
+	onShow() {},
+	bindNext() {
+		let i = this.data.shareIndex + 1
+		if (i === 5) {
+			i = 1
+		}
+		this.setData({
+			shareIndex: i,
+		})
+	},
+	bindHoliday(){
+		Sea.path('/pages/holiday/holiday')
+	},
+})
