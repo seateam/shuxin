5734e5893a63b5dd4cd6e93d668719da4951b9a2
bigsea <iwangyang@qq.com>
2019-06-03 20:09:59 +0800

    自定义组件

--- app/packages/navBar/navBar.js
+++ app/packages/navBar/navBar.js
@@ -4,13 +4,26 @@ Component({
 	options: {
 		addGlobalClass: false,
 	},
+	properties: {
+		theme: {
+			type: String,
+		},
+	},
+	observers: {
+		theme (value) {
+			console.log('🐸', value)
+		}
+	},
 	data: {
 		navBar: app.data.navBar,
 	},
-	attached() {},
+	attached () {
+		console.log('🐸', this.data.theme)
+	},
 	methods: {
-		bindBack() {
+		bindBack () {
 			Sea.back()
+			console.log('🐸', this.data.theme)
 		},
 	},
 })
