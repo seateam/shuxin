488f34825ca54da3e6bfa71508d83a2bf4285eab
bigsea <iwangyang@qq.com>
2019-07-02 16:59:44 +0800

    css

--- app/packages/sea-group-comment/group-comment.js
+++ app/packages/sea-group-comment/group-comment.js
@@ -19,14 +19,22 @@ Component({
             'http://statics03.qingmang.mobi/image/proxy/aHR0cHMlM0EvL3d4LnFsb2dvLmNuL21tb3Blbi92aV8zMi9QaWFqeFNxQlJhRUlJN2EwY2ZIYVpmTDNiQ2pYbzF5WDFURzNSaWMxU3ZEOUdpY3VYR2ljemN3bk9ham9IR2plZGJ3bDlKRHBhQlFhTkpnUEIwaFFkcUJiOFEvMTMy',
         },
         text: '两年老员工报以礼貌的微笑~',
-        imgs: ['http://statics01.qingmang.mobi/FuY-Srn-SOeREBBxzcdQ1QL0LEHx'],
+        imgs: [
+          'http://statics01.qingmang.mobi/FuY-Srn-SOeREBBxzcdQ1QL0LEHx',
+          'http://statics01.qingmang.mobi/FjuLuRCxNtV06IA7yCS6MkmilPJU',
+          'http://qiniuimg.qingmang.mobi/image/orion/3e0d6732a1f7bc273f2f4b6c7ceea3b4_945_628.jpeg?imageView2/2/w/750',
+          'http://qiniuimg.qingmang.mobi/image/orion/06a7b91f1103407fdb0bfeb7994d37d1_1080_703.jpeg?imageView2/2/w/750',
+          'http://qiniuimg.qingmang.mobi/image/orion/aa08370f4fcf79e7ca097946d36eec77_900_500.jpeg?imageView2/2/w/750',
+          'http://qiniuimg.qingmang.mobi/image/orion/105f710672bc2d2c055008a7a1c779f7_1024_768.jpeg?imageView2/2/w/750',
+          'http://qiniuimg.qingmang.mobi/image/orion/6c2e0a49f0912224d0047dbbcb5752b9_640_396.jpeg',
+        ],
         like: [
           'http://statics03.qingmang.mobi/image/proxy/aHR0cHMlM0EvL3d4LnFsb2dvLmNuL21tb3Blbi92aV8zMi9EWUFJT2dxODNlcEd4Y29SaWI5aWE3SGdXNWN3akdEbEZrdDhUVExxRmxKaHJtMm5oTmJaQUpkdUMzMlJpYjV1M29RTHBHWlEzSXMyQ09weFh3aWNjUmdNNEEvMTMy',
           'http://statics03.qingmang.mobi/image/proxy/aHR0cCUzQS8vdGhpcmR3eC5xbG9nby5jbi9tbW9wZW4vdmlfMzIvRFlBSU9ncTgzZXBSNXI2eG53MWJXVm1rNno4NUNMRmpjMUhmOUV3Y1NhVkFQcWliZ3ljZmFYYnF2ZW90T2RCVVhNeWtkcGFjY0hOWGZtV1lOcG92OHVnLzEzMg==',
           'http://statics03.qingmang.mobi/image/proxy/aHR0cHMlM0EvL3d4LnFsb2dvLmNuL21tb3Blbi92aV8zMi9EWUFJT2dxODNlcWVPWTZDQUdRb1l3bFlUZklZNExCdFh6cHFDcmljNEIzMkU2bUw0dE15WE9zdWlidWlidmV0SHRkQWljYXBQMUR1M1BNY09BWnZVRVZHSFEvMTMy',
           'http://statics03.qingmang.mobi/image/proxy/aHR0cHMlM0EvL3d4LnFsb2dvLmNuL21tb3Blbi92aV8zMi9QaWFqeFNxQlJhRUxyY2lhOUQ1NHJmQUIycXphRFlqb1ZJRXlpY2ljczRUdHFGcXJoNXgwaWN2NlA4R090OWxKazZpYmdxNXA1UmVNUlU2RzR5VHNZNlVQM0h1dy8xMzI=',
         ],
-        iLike: true, 
+        iLike: true,
         comments: [
           {
             who: {
@@ -51,7 +59,8 @@ Component({
               sponsor: true,
             },
             at: {
-              name: '嗯 这个话题来眠的夜晚，似乎我们都是不同程度的『麻烦制造者』。当然学校也里有很多让人觉得麻烦的事：为什么宿舍要断电断网、为什么大学还要早读、为什么不允许我们自由表达？ 这是一个长期讨论的话题，欢迎在留言里写下任何你的想法或思考。我们会探索不同的呈现形式，把这些内容呈现出来，比如：表情包、四格漫画、插图等。 #壹周编辑部 ',
+              name:
+                '嗯 这个话题来眠的夜晚，似乎我们都是不同程度的『麻烦制造者』。当然学校也里有很多让人觉得麻烦的事：为什么宿舍要断电断网、为什么大学还要早读、为什么不允许我们自由表达？ 这是一个长期讨论的话题，欢迎在留言里写下任何你的想法或思考。我们会探索不同的呈现形式，把这些内容呈现出来，比如：表情包、四格漫画、插图等。 #壹周编辑部 ',
             },
             text: 'dameng',
           },
