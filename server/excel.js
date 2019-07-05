bdf64701d920eb1e3867078609ed7d5f2bf0483c
bigsea <iwangyang@qq.com>
2019-07-06 00:45:48 +0800

    德玛西亚

--- server/excel.js
+++ server/excel.js
@@ -1,4 +1,4 @@
-const log = console.log.bind('>>>', console)
+const log = console.log.bind(console, '>>>')
 const xlsx = require('node-xlsx')
 const axios = require('axios')
 const Qs = require('qs')
@@ -14,7 +14,7 @@ const who = function(name) {
     if (openid && name === user_name) {
       for (const e2 of e.data.slice(1)) {
         let [content, location, time_stamp, location_text] = e2
-        if ((content, location, time_stamp, location_text)) {
+        if (content && location && time_stamp && location_text) {
           arr.push(e2)
         }
       }
@@ -24,18 +24,15 @@ const who = function(name) {
 }
 
 // 注意 这里会写入数据
-// openid
-const dict = {
-  组长: 'o6iD25X_2wwl5CnLnoHsmLYPHJ3s',
-  大海: 'o6iD25b9G_ORHJiGWm4uhnjGafkc',
-  不圆: 'o6iD25VhzkWx-uk2xDszuyD3yhiQ',
-  鱼雷: 'o6iD25fqF5FKDQ--9bMJP9zXs3Y4',
-  小海: 'o6iD25ZN-qlcbUMBicYO1EN0tKbA',
-}
-const openid = dict['不圆']
+// 小海 o6iD25ZN-qlcbUMBicYO1EN0tKbA
+// 祖哥 o6iD25VhzkWx-uk2xDszuyD3yhiQ
+// A龟 o6iD25X_2wwl5CnLnoHsmLYPHJ3s
+// B海 o6iD25b9G_ORHJiGWm4uhnjGafkc
+// C韩 o6iD25fqF5FKDQ--9bMJP9zXs3Y4
+const openid = 'o6iD25fqF5FKDQ--9bMJP9zXs3Y4'
 // 生成 url
 const request = []
-for (const e of who('祖哥')) {
+for (const e of who('C韩')) {
   let [content, location, time_stamp, location_text] = e
   let data = {
     content: content,
@@ -50,19 +47,27 @@ for (const e of who('祖哥')) {
   request.push(url)
 }
 // 递龟请求
-const cardAdd = function(url) {
-  axios(url).then((res) => {
-    if (res.data.ok) {
-      log('写入成功')
-    } else {
-      log('写入失败')
-    }
-    // 取出
-    let url = request.splice(0, 1)[0]
-    if (url) {
-      cardAdd(url)
-    }
-  })
-}
-const url = request.splice(0, 1)[0]
-cardAdd(url)
+// const cardAdd = function(url) {
+//   axios(url).then((res) => {
+//     if (res.data.ok) {
+//       log('写入成功')
+//     } else {
+//       log('写入失败')
+//     }
+//     // 取出
+//     let url = request.splice(0, 1)[0]
+//     if (url) {
+//       setTimeout(() => {
+//         cardAdd(url)
+//       }, 5000)
+//     }
+//   })
+// }
+// const url = request.splice(0, 1)[0]
+// cardAdd(url)
+
+// 查询
+// axios('https://api.echo1999.com/v1/card.get?openid=' + openid).then((res) => {
+//   for (const e of res.data.data) {
+//   }
+// })
